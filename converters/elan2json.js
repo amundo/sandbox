5b390a6bec2abca5902b24000110f45109959ec4
Patrick Hall <pjh@umail.ucsb.edu>
2015-02-23 09:58:32 -0800

    another little script for converting elan tsv

---
+++ converters/elan2json.js
@@ -0,0 +1,19 @@
+var lines = before.value.trim().split('\n');
+
+var headers = 'startTime endTime duration transcript notes translation phonemic phonetic'.split(' ');
+
+
+function labelLine(line) {
+  var o = {};
+  var parts = line.split('\t');
+  headers.forEach(function (header, i) {
+    o[header] = parts[i]
+  }); 
+  return o;
+}
+
+
+
+after.value = JSON.stringify(lines.slice(1,lines.length).map(labelLine), null, 2);
+
+
