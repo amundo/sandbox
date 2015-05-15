7ab2fcf82d4aa51f2d1c3a14b5d0f4d2341588ab
Patrick Hall <pjh@umail.ucsb.edu>
2015-05-15 16:12:00 -0700

    first step to modeling

---
+++ modeling/js/modeling.js
@@ -0,0 +1,39 @@
+function frequency(seuqence){
+  var tally = {};
+ 
+  sequence.reduce((tally, current) => {
+    tally[current] ? tally[current] += 1 : tally[current] = 0;
+    return tally;
+  }, {})
+
+  return tally;
+}
+
+// guess which transcription words align with which translation words
+function guess(text, from, to){
+  var 
+    alignment = {},
+    transcriptions = text.phrases.map(p => p.transcriptions[from]),
+    translations = text.phrases.map(p => p.translations[to]);
+  
+  transcriptions.forEach((transcription, i) => {
+    var tokens = transcription.split(' '),
+        translationWords = translations[i].split(' ');
+    
+    tokens.forEach(function(token){
+      translationWords.forEach(function(translationWord){
+        if(!(token in alignment)) { alignment[token] = {} }
+        alignment[token][translationWord] ? alignment[token][translationWord] += 1 : alignment[token][translationWord] =  1
+      })
+    }) 
+  
+  })
+
+  
+  return alignment;
+}
+                       
+
+//guess(text);
+
+
---
+++ modeling/js/test/spec/ModelingSpec.js
@@ -0,0 +1,68 @@
+describe('Modeling', function(){
+  beforeEach(function(){
+
+    var lexicon = [];
+  
+    var text = {
+      metadata: { 
+        title: "Sample Parallel Text"
+      },
+      phrases : [
+        {
+          translations:   { en: "what is your name" },
+          transcriptions: { id: "siapa nama anda" }
+        },
+        { 
+          translations:   { en: "my name is" },
+          transcriptions: { id: "nama saya " }
+        },
+        {
+          translations:   { en: "where are you from" },
+          transcriptions: { id: "anda berasal dari mana" }
+        },
+        { 
+          translations:   { en: "what is new"}, 
+          transcriptions: { id: "apa kabar" } 
+        },
+        {
+          transcriptions: { id: "good morning" },
+          translations:   { en: "selamat pagi" }
+        },
+        {
+          transcriptions: { id: "good afternoon" },
+          translations:   { en: "selamat siang" }
+        },
+        {
+          transcriptions: { id: "good evening" },
+          translations:   { en: "selamat sore" }
+        },
+        {
+          transcriptions: { id: "good night" },
+          translations:   { en: "selamat malam" }
+        }
+      ] 
+    };
+
+    this.alignment = guess(text, 'id', 'en');
+
+  })
+
+  describe('Text Alignment', function(){
+
+    it('exists ', function(){
+      expect(this.alignment).not.toBe(null)
+    })
+
+    it('model of good ', function(){
+      expect(this.alignment['good']).toEqual({ "selamat": 4, "pagi": 1, "siang" : 1, "sore" :1, "malam": 1})
+    })
+
+    it('best for good is selamat', function(){
+      expect(this.alignment['good']).toEqual({ "selamat": 4, "pagi": 1, "siang" : 1, "sore" :1, "malam": 1})
+    })
+
+
+  })
+
+
+})
