c227d43659e892936b2f28ec35a60c7f068056b9
Patrick Hall <pjh@umail.ucsb.edu>
2015-05-30 15:25:19 -0700

    try to fix cursor to location after transliteration

--- transliterationInput/js/transcriptionView.js
+++ transliterationInput/js/transcriptionView.js
@@ -2,43 +2,47 @@ var Transliterator = function(rules){
   var escapeRegExp = function(unescaped){
     return (unescaped).replace(/[.?*+^$[\]\\(){}|-]/g, "\\$&");
   }
+
+  var unescapeUPlus = function(side){
+    if(side.startsWith('U+')){ 
+      var codePoint = side.split('U+')[1];
+      return String.fromCodePoint(parseInt(codePoint, 16));
+    } else { 
+      return side
+    }
+  }
+
   this.transliterate = function(text){
     rules.forEach(function(rule){ 
       var before = new RegExp(escapeRegExp(rule.before), 'g');
-      text = text.replace(before, rule.after) 
+      var after = unescapeUPlus(rule.after);
+      
+      text = text.replace(before, after);
     })
-    return text;
+    return text.normalize('NFC');
   }
 }
 
 var TranscriptionView = function(options){
   var 
     el = options.el,
-    input = el.querySelector('input'),
+    input = options.box,
     toggleRulesButton = el.querySelector('button'),
     ruleList = el.querySelector('textarea#ruleList'),
     rules = [];
 
-
   this.el = el;
-  this.processRules = function(){
+
+  toggleRulesButton.addEventListener('click', function(){
+    ruleList.classList.toggle('hidden');
+  })
+
+  this.processRulesText = function(){
     var rules = ruleList.value.trim().split('\n');
 
     rules = rules.map(function(rule){
          return rule.split(/[ \t]/g).map(String.trim)
       })
-
-    rules = rules.map(function(rule){
-      return rule.map(function(side,i){
-        if(side.startsWith('U+')){
-          var codePoint = side.split('U+')[1];
-          var unicodeString = String.fromCodePoint(parseInt(codePoint, 16));
-          return unicodeString
-        } else {
-          return side
-        } 
-      })
-    })
 
     var rules = rules.map(function(rule){
       var rule = { 
@@ -52,17 +56,19 @@ var TranscriptionView = function(options){
     return rules
   }
 
-  toggleRulesButton.addEventListener('click', function(){
-    ruleList.classList.toggle('hidden');
-  })
-
   this.transliterate = function(text){
-    var transliterator = new Transliterator(this.processRules())
+    var transliterator = new Transliterator(this.processRulesText())
     return transliterator.transliterate(text)
   }
 
   input.addEventListener('input', function(ev){
+    var start = ev.target.selectionStart,
+        end = ev.target.selectionEnd;
+
     ev.target.value = this.transliterate(ev.target.value);
+
+    ev.target.setSelectionRange(end, end);
+
   }.bind(this))
   
 }
